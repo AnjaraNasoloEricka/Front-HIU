93197c0a86bcde054a0ad34c8b7e129e7b16dd1c
Cedric Rabarijohn <cedrictianarabarijohn@gmail.com>
2023-03-08 17:34:14 +0300

    Change exam

--- matx-react-master/src/app/views/exams/Exams.jsx
+++ matx-react-master/src/app/views/exams/Exams.jsx
@@ -1,6 +1,6 @@
 import { Box, styled } from "@mui/material";
 import { getExamList } from "app/apis/examApi";
-import { Breadcrumb, SimpleCard } from "app/components";
+import { Breadcrumb, MatxLoading, SimpleCard } from "app/components";
 import PaginationTableExam from "app/components/MatxTable/PaginationTableExam";
 import { useEffect, useState } from "react";
 
@@ -12,61 +12,63 @@ const Container = styled("div")(({ theme }) => ({
     [theme.breakpoints.down("sm")]: { marginBottom: "16px" },
   },
 }));
-
-const examListMock = [
-  {
-    _id: "64083912c470860f411d7a26",
-    etudiantId: "64063960c2f732da57d49878",
-    date_debut: "2023-03-08T10:00:00.000Z",
-    date_fin: "2023-03-08T11:00:00.000Z",
-    matiere: "Web Dynamique 1",
-    theme: "theme",
-  },
-  {
-    _id: "64083912c470860f411d7a26",
-    etudiantId: "64063960c2f732da57d49878",
-    date_debut: "2023-01-01 12:00:00",
-    date_fin: "2023-01-01 13:00:00",
-    matiere: "Web Dynamique 2",
-    theme: "Servlet",
-  },
-  {
-    _id: "64083912c470860f411d7a26",
-    etudiantId: "64063960c2f732da57d49878",
-    date_debut: "2023-01-01 12:00:00",
-    date_fin: "2023-01-01 13:00:00",
-    matiere: "Web Dynamique 3",
-    theme: "Servlet",
-  },
-  {
-    _id: "64083912c470860f411d7a26",
-    etudiantId: "64063960c2f732da57d49878",
-    date_debut: "2023-01-01 12:00:00",
-    date_fin: "2023-01-01 13:00:00",
-    matiere: "Web Dynamique 4",
-    theme: "Servlet",
-  },
-];
+// const examListMock = [
+//   {
+//     _id: "64083912c470860f411d7a26",
+//     etudiantId: "64063960c2f732da57d49878",
+//     date_debut: "2023-03-08T10:00:00.000Z",
+//     date_fin: "2023-03-08T11:00:00.000Z",
+//     matiere: "Web Dynamique 1",
+//     theme: "theme",
+//   },
+//   {
+//     _id: "64083912c470860f411d7a26",
+//     etudiantId: "64063960c2f732da57d49878",
+//     date_debut: "2023-01-01 12:00:00",
+//     date_fin: "2023-01-01 13:00:00",
+//     matiere: "Web Dynamique 2",
+//     theme: "Servlet",
+//   },
+//   {
+//     _id: "64083912c470860f411d7a26",
+//     etudiantId: "64063960c2f732da57d49878",
+//     date_debut: "2023-01-01 12:00:00",
+//     date_fin: "2023-01-01 13:00:00",
+//     matiere: "Web Dynamique 3",
+//     theme: "Servlet",
+//   },
+//   {
+//     _id: "64083912c470860f411d7a26",
+//     etudiantId: "64063960c2f732da57d49878",
+//     date_debut: "2023-01-01 12:00:00",
+//     date_fin: "2023-01-01 13:00:00",
+//     matiere: "Web Dynamique 4",
+//     theme: "Servlet",
+//   },
+// ];
 const Exams = () => {
   const [exams, setExams] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const initializeExamsList = () => {
-    setIsLoading(true)
-    getExamList().then(e=>{
-      if(e.exams){
-        setExams(e.exams)
-      }
-    }).catch(err=>{
-      console.log(err)
-    }).finally(()=>{
-      setIsLoading(false)
-    })
+    setIsLoading(true);
+    getExamList()
+      .then((e) => {
+        if (e.exams) {
+          setExams(e.exams);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
     // setExams(examListMock);
   };
   useEffect(() => {
     initializeExamsList();
   }, []);
-  
+
   return (
     <div>
       <Container>
@@ -74,7 +76,11 @@ const Exams = () => {
           <Breadcrumb routeSegments={[{ name: "Examen", path: "/Examen" }]} />
         </Box>
         <SimpleCard title="Liste de mes examens à venir / en cours">
-          <PaginationTableExam examlist={exams} />
+          {isLoading ? (
+            <MatxLoading />
+          ) : (
+            <PaginationTableExam examlist={exams} />
+          )}
         </SimpleCard>
       </Container>
     </div>
